5cb1a0422dbf28b1d971d213732f70f4e456e7f9
Evaisa <evagiacosa1@gmail.com>
2023-09-20 20:14:13 +0200

    implement music magic fix

---
+++ data/moremusicalmagic/songs_chemical.lua
@@ -0,0 +1,9 @@
+
+ocarina_songs["tinker"] = { "a2", "f", "d", "b", "a", "c", "e", "gsharp", "a2" }  --Hinted at in desert chasm
+
+ocarina_funcs["tinker"] = function()                  --Note: This is infinitely reusable and has no cooldown
+  local x, y = EntityGetTransform( entity_id )
+  EntityLoad( "mods/Hydroxide/files/chemical_curiosities/materials/tinkerDust/tinkersong.xml", x, y )
+  GamePrintImportant( "The Gods sing along to your song", "Can you hear them?" )
+end
+
