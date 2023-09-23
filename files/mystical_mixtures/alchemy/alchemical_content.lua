1afd03c444d15a01d55473b7a8a54b27a1fc7d85
Evaisa <evagiacosa1@gmail.com>
2023-09-23 13:16:23 +0200

    improve alchemy system

--- files/mystical_mixtures/alchemy/alchemical_content.lua
+++ files/mystical_mixtures/alchemy/alchemical_content.lua
@@ -1,27 +1,82 @@
 alchemical_materials = {
     {
-        id = "test_liquid", -- id of the material
-        name = "Test Liquid", -- name of the material
-        color = "FFA38D3C", -- visual color
+        id = "alchemical_solvent", -- id of the material
+        name = "Alchemical Solvent", -- name of the material
+        description = "Alchemical solvent is a material which is used to create mystical solutions.",
+        generate_notes = true,
+        color = "FF72BFD8", -- visual color
         texture = nil, -- texture path
-        type = "liquid", -- liquid, powder, gas
+        type = "liquid",
+        tags = "",
+    },
+    {
+        id = "alchemical_base",
+        name = "Alchemical Base",
+        description = "The base of all proper alchemy.",
+        generate_notes = true,
+        color = "c87e6996",
+        texture = nil, -- texture path
+        type = "liquid",
+        tags = "",       
+    },
+    {
+        id = "ephemeral_ether",
+        name = "Ephemeral Ether",
+        description = "A voletile substance which evaporates quickly.\nA side product of many alchemical processes.\nUses are yet unknown.",
+        generate_notes = true,
+        color = "C60AFFD2",
+        texture = nil, -- texture path
+        type = "liquid",
+        tags = "",
+    },
+    {
+        id = "ephemeral_ether_gas",
+        name = "Ephemeral Ether Gas",
+        description = "The evaporated form of Ephemeral Ether.",
+        color = "410AFFD2",
+        texture = nil, -- texture path
+        type = "gas",
+        tags = "",
+        lifetime = 200,
+        burnable = true,
+        glow = 20,
     }
 }
 
 alchemical_recipes = {
     {
-        id = "improve_slots",
-        name = "On the subject of wand slots",
-        description = "test",
+        id = "ether_evaporation",
+        probability = 1, 
+        inputs = {
+            "ephemeral_ether",
+            "air",
+        },
+        outputs = {
+            "ephemeral_ether_gas",
+            "air",
+        },
+        func = nil
+    },
+    {
+        id = "gold_solution",
+        name = "Solution of Gold",
+        description = "Gold can be dissolved in an alchemical solvent to be more usable in future recipes.",
+        generate_notes = true,
         probability = 100, 
         inputs = { -- three ingredients is the limit
             "[gold]",
-            "alchemical_base",
+            "alchemical_solvent",
+            "[fire]",
         },
         outputs = {
             "air",
-            "test_liquid",
+            "gold_solution",
         },
-        func = nil -- function(x, y)
+        blob_radius1 = nil,
+        blob_radius2 = nil,
+        blob_radius3 = nil,
+        func = function(x, y)
+            
+        end
     }
 }
--- files/mystical_mixtures/alchemy/generate_content.lua
+++ files/mystical_mixtures/alchemy/generate_content.lua
@@ -1,22 +1,6 @@
 dofile("mods/Hydroxide/files/mystical_mixtures/alchemy/alchemical_content.lua")
 
 local materials_xml = [[<Materials>
-    <CellData 
-        name="alchemical_base"
-        ui_name="Alchemical Base"
-        tags="[liquid]"
-        density="2.9"
-        cell_type="liquid"
-        wang_color="ff706747"
-        gfx_glow="0"
-        liquid_gravity="0.6"
-        liquid_stains="1"
-        >
-        <Graphics
-            color="c87e6996"
-        >
-        </Graphics>
-    </CellData>
 ]]
 
 for k, v in ipairs(alchemical_materials)do
@@ -25,13 +9,15 @@ for k, v in ipairs(alchemical_materials)do
     <CellData 
         name="]]..v.id..[["
         ui_name="]]..v.name..[["
-        tags="[liquid]"
+        tags="]]..(v.tags ~= nil and (v.tags .. ",[liquid]") or "[liquid]")..[["
+        burnable="]]..(v.burnable and "1" or "0")..[["
         density="2.9"
         cell_type="liquid"
         wang_color="]]..v.color..[["
-        gfx_glow="0"
+        gfx_glow="]]..(v.glow ~= nil and v.glow or 0)..[["
         liquid_gravity="0.6"
         liquid_stains="1"
+        lifetime="]]..(v.lifetime ~= nil and v.lifetime or -1)..[["
         >
         <Graphics
             color="]]..v.color..[["
@@ -39,6 +25,60 @@ for k, v in ipairs(alchemical_materials)do
         >
         </Graphics>
     </CellData>
+        ]]
+    elseif(v.type == "powder")then
+        materials_xml = materials_xml .. [[
+        <CellData
+            name="]]..v.id..[["
+            ui_name="]]..v.name..[["
+            tags="]]..(v.tags ~= nil and (v.tags .. ",[sand_other],[corrodible]") or "[sand_other],[corrodible]")..[["
+            burnable="]]..(v.burnable and "1" or "0")..[["
+            density="4.5"
+            cell_type="liquid"
+            wang_color="]]..v.color..[["
+            generates_smoke="0"
+            liquid_gravity="0.2"
+            liquid_sand="1"
+            on_fire="0"
+            requires_oxygen="0"
+            audio_physics_material_event="sand"
+            audio_physics_material_wall="sand"
+            audio_physics_material_solid="sand"
+            show_in_creative_mode="1"
+            gfx_glow="]]..(v.glow ~= nil and v.glow or 0)..[["
+            lifetime="]]..(v.lifetime ~= nil and v.lifetime or -1)..[["
+            >
+            <Graphics
+                color="]]..v.color..[["
+                ]]..((v.texture ~= nil) and ("texture_file="..v.texture) or "") ..[[
+            >
+            </Graphics>
+        </CellData>     
+        ]]
+    elseif(v.type == "gas")then
+        materials_xml = materials_xml .. [[
+        <CellData
+            name="]]..v.id..[["
+            ui_name="]]..v.name..[["
+            tags="]]..(v.tags ~= nil and (v.tags .. ",[gas]") or "[gas]")..[["
+            burnable="]]..(v.burnable and "1" or "0")..[["
+            density="1"
+            lifetime="]]..(v.lifetime ~= nil and v.lifetime or -1)..[["
+            cell_type="gas"
+            wang_color="]]..v.color..[["
+            generates_smoke="0"
+            liquid_gravity="0.1"
+            liquid_sand="0"
+            on_fire="0"
+            requires_oxygen="0"
+            gfx_glow="]]..(v.glow ~= nil and v.glow or 0)..[["
+            >
+            <Graphics
+                color="]]..v.color..[["
+                ]]..((v.texture ~= nil) and ("texture_file="..v.texture) or "") ..[[
+            >
+            </Graphics>
+        </CellData> 
         ]]
     end
 end
@@ -98,6 +138,7 @@ for k, v in ipairs(alchemical_recipes)do
     <Reaction probability="]]..tostring(v.probability or 100)..[["
         input_cell1="]]..input1..[["	input_cell2="]]..input2..[["	input_cell3="]]..(input3 or "")..[["
         output_cell1="]]..output1..[["	output_cell2="]]..output2..[["	output_cell3="]]..(output3 or "")..[["
+        blob_radius1="]]..(v.blob_radius1 or "")..[[" blob_radius2="]]..(v.blob_radius2 or "")..[[" blob_radius3="]]..(v.blob_radius3 or "")..[["
     ]]
 
     if(v.func ~= nil)then 
@@ -105,6 +146,37 @@ for k, v in ipairs(alchemical_recipes)do
         entity="data/mystical_mixtures/alchemy/]]..v.id..[[.xml"
     />
     ]]
+    
+    local entity_xml = [[
+    <Entity name="MM_alchemy_handler">
+        <LifetimeComponent
+            lifetime="2"
+        />
+        <LuaComponent
+            script_source_file="data/mystical_mixtures/alchemy/scripts/]]..v.id..[[.lua"
+            execute_on_added="1"
+            execute_every_n_frame="0"
+            execute_times="-1"
+        />
+    </Entity>
+    ]]
+
+    ModTextFileSetContent("data/mystical_mixtures/alchemy/"..v.id..".xml", entity_xml)
+
+    local script_lua = [[
+        dofile("mods/Hydroxide/files/mystical_mixtures/alchemy/alchemical_content.lua")
+        local entity_id = GetUpdatedEntityID()
+        local x, y = EntityGetTransform(entity_id)
+        local recipe_id = "]]..v.id..[["
+        for k, v in ipairs(alchemical_recipes)do
+            if(v.id == recipe_id)then
+                v.func(x, y)
+            end
+        end
+    ]]
+
+    ModTextFileSetContent("data/mystical_mixtures/alchemy/scripts/"..v.id..".lua", script_lua)
+
     else
         materials_xml = materials_xml .. [[
     />]]
