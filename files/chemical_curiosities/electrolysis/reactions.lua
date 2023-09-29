6da606ef67636c9bec9e99d7458806ba7601721d
UserK <111083281+FatalUserK@users.noreply.github.com>
2023-09-29 02:02:45 +0100

    added antimatter electrolysis reaction

--- files/chemical_curiosities/electrolysis/reactions.lua
+++ files/chemical_curiosities/electrolysis/reactions.lua
@@ -11,4 +11,10 @@ reactions = {
         probability = 100, -- likelihood of reaction happening when a checker is touched by electricity
         radius = 5, -- How big an area it converts
     },
+    {
+        input = "cc_antimatter_powder",
+        output = "cc_antimatter_gas",
+        probability = 100, -- likelihood of reaction happening when a checker is touched by electricity
+        radius = 30, -- How big an area it converts
+    }, -- "my son would never blatantly copy-paste code without removing comments"
 }
