e2f0395d2ff69ce9218efcbe86300993d6e325c7
UserK <111083281+FatalUserK@users.noreply.github.com>
2023-09-30 05:12:58 +0100

    minor changes
    fixed methane shader applying when non-player entities are stained
    
    modified methane to make it a bit more common (still in testing phase, not final)
    
    fixed methane colour (subject to change)

--- files/chemical_curiosities/materials/methane/effect_controller.lua
+++ files/chemical_curiosities/materials/methane/effect_controller.lua
@@ -4,6 +4,10 @@ dofile_once("mods/Hydroxide/files/lib/status_helper.lua")
 local entity_id = GetUpdatedEntityID()
 local owner = EntityGetParent(entity_id)
 
+if(not EntityHasTag(owner, "player_unit"))then
+    return
+end
+
 local count = GetStainPercentage(owner, "INGESTION_METHANE")
 
 local multiplier = (ModSettingGet("Hydroxide.CC_METHANE_EFFECT_MULTIPLIER") or 100) / 100
@@ -13,5 +17,5 @@ count = count * multiplier
 GameSetPostFxParameter("grayscale", 0, 0, 0, count)
 
 if (count <= 0) then
-	EntityKill(entity_id)
+    EntityKill(entity_id)
 end
