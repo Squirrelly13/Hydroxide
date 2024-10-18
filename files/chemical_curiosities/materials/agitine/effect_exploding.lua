a430fe3e1d803fd6f6a1f4db24c6b4d10810d937
UserK <111083281+FatalUserK@users.noreply.github.com>
2024-10-18 16:37:08 +0100

    Update effect_exploding.lua

--- files/chemical_curiosities/materials/agitine/effect_exploding.lua
+++ files/chemical_curiosities/materials/agitine/effect_exploding.lua
@@ -21,7 +21,8 @@ local do_explosion = ComponentGetValue2(vscomp, "value_bool") --grab the current
 
 local a = GetStainPercentage(owner, "CC_EXPLODING") --Get Stain% as value from 0 to 1
 local b = GetIngestionSeconds(owner, "CC_EXPLODING") --Get Ingestion Seconds (1:1 in terms of seconds, 10% of flask = 30 seconds)
-local multiplier = ((a + .3)^2.7 * 2) + (b^1.2 * .01) --Combine them using some fun mildly-exponential math stuff (healvily decrease Ingestion to avoid it completely throwing off the Stain%)
+
+local multiplier = ((a + .3)^2.7 * 1.5) + (b^1.2 * .02) --Combine them using some fun mildly-exponential math stuff (healvily decrease Ingestion to avoid it completely throwing off the Stain%)
 
 
 --logic for if there should be an explosion:
@@ -36,7 +37,7 @@ elseif ComponentGetValue2(vscomp, "value_int") < 5 and b + previous_ingestion ~=
     ComponentSetValue2(vscomp, "value_int", counter + 1) --increment the counter by 1
     return --exit script
 
-elseif do_explosion == false then --effect should run every 2 seconds but the script runs every 1 second, so i have a script 
+elseif b == 0 and do_explosion == false then --effect should run every 2 seconds but the script runs every 1 second, so i have a script 
     ComponentSetValue2(vscomp, "value_bool", true)
     return --exit script
 
@@ -56,15 +57,15 @@ if not comp then return end
 --Apply the multiplier in various ways to the explosion stats (i spent like an entire day tweaking these and im still not full satisfied :devastatated:)
 ComponentObjectSetValue2(comp, "config_explosion", "camera_shake", 13 * math.min(20, multiplier)) --how much the camera shakes
 ComponentObjectSetValue2(comp, "config_explosion", "damage", 4 * multiplier^1.5) --damage (uncapped cuz funny and no reason to limit it)
-ComponentObjectSetValue2(comp, "config_explosion", "explosion_radius", 16 * math.min(40, multiplier^.8)) --radius of the explosion (obviously limited for good reasons)
+ComponentObjectSetValue2(comp, "config_explosion", "explosion_radius", 12 * math.min(50, multiplier^.8)) --radius of the explosion (obviously limited for good reasons)
 ComponentObjectSetValue2(comp, "config_explosion", "ray_energy", 20000 * math.min(1200, multiplier^1.8)) --the strength of the exposion (damage it does to pixels)
 ComponentObjectSetValue2(comp, "config_explosion", "who_is_responsible", owner) --identify the explosion origin (so they get kill credit probs)
-ComponentObjectSetValue2(comp, "config_explosion", "sparks_count_min", (6 * math.min(20, multiplier))^2) --minimum particle sparks
-ComponentObjectSetValue2(comp, "config_explosion", "sparks_count_max", (13 * math.min(20, multiplier))^2) --maximum particle sparks
+ComponentObjectSetValue2(comp, "config_explosion", "sparks_count_min", (5 * math.min(20, multiplier))^1.4) --minimum particle sparks
+ComponentObjectSetValue2(comp, "config_explosion", "sparks_count_max", (12 * math.min(20, multiplier)^2)) --maximum particle sparks
 ComponentObjectSetValue2(comp, "config_explosion", "max_durability_to_destroy", 14) --ensures this can break through high-level materials if it has enough ray_energy
 
 if GameHasFlagRun("PERK_PICKED_IRON_STOMACH") and b ~= 0 then --if player has picked IRON_STOMACH and Ingestion is not 0:
-    ComponentObjectSetValue2(comp, "config_explosion", "dont_damage_this", owner) --set the explosion to not harm the target
+    ComponentObjectSetValue2(comp, "config_explosion", "dont_damage_this", owner) --set the explosion to not harm the owner
 end
 EntityRemoveIngestionStatusEffect(owner, "CC_EXPLODING") --Remove all of the ingested effect (leaves the stain)
 ComponentSetValue2(vscomp, "value_bool", false) --reset the do_explosion bool so the effect continues to only take effect every 2 seconds
