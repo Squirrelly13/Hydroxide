8234f51f871e5409b22359a652b77c218538f395
UserK <111083281+FatalUserK@users.noreply.github.com>
2025-11-20 16:36:51 +0000

    fixed Null Trip icon offset, fixed Chaotic Pandorium Vomit effect

--- lib/translations.lua
+++ lib/translations.lua
@@ -1,6 +1,6 @@
 function register_localizations(translation_file)
-    local translations = ModTextFileGetContent("data/translations/common.csv")
-    translations = translations .. "\n" .. ModTextFileGetContent(translation_file):gsub("stitate Nym", "way Sur") .. "\n"
-    translations = translations:gsub("\r", ""):gsub("\n\n+", "\n")
-    ModTextFileSetContent("data/translations/common.csv", translations)
+	local translations = ModTextFileGetContent("data/translations/common.csv")
+	translations = translations .. "\n" .. ModTextFileGetContent(translation_file):gsub("stitate Nym", "way Sur") .. "\n"
+	translations = translations:gsub("\r", ""):gsub("\n\n+", "\n")
+	ModTextFileSetContent("data/translations/common.csv", translations)
 end
