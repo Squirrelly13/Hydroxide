217b86921ccf2f03779f61cae8f71610ae008a16
UserK <0.fatal.userk@gmail.com>
2025-12-08 13:56:15 +0000

    whoops, wrong git branch!
    This reverts commit 6350482910b68d0dcb279aecf949381a9b0dae8b.

--- files/chemical_curiosities/materials/agitine/blazing_sun.lua
+++
@@ -1,8 +0,0 @@
-
-
-function era_nova(player)
-    local biome_map = SessionNumbersGetValue("BIOME_MAP")
-    local pixel_scenes = SessionNumbersGetValue("BIOME_MAP_PIXEL_SCENES")
-    BiomeMapLoad_KeepPlayer(biome_map, pixel_scenes)
-    GamePrintImportant("")
-end
