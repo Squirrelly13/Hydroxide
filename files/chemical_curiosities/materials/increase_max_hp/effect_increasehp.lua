fd95194da33465b4f25a919892ac823a325df822
UserK <0.fatal.userk@gmail.com>
2025-12-21 13:04:11 -0600

    rebalanced Health Tonic
    HP now increases 1% every 60 frames rather than every 10 frames
    used math to make the increase apply gradually every frame but you will still see an overall increase of 1% every 60 frames
    increased how much of the effect you get from drinking Health Tonic. a full flask will give you 60 seconds, leading to a ~81% increase in Max HP from a single flask

--- files/chemical_curiosities/materials/increase_max_hp/effect_increasehp.lua
+++ files/chemical_curiosities/materials/increase_max_hp/effect_increasehp.lua
@@ -2,6 +2,12 @@ local entity = GetUpdatedEntityID()
 local root = EntityGetParent(entity)
 
 for _,damagemodel in ipairs(EntityGetComponent(root, "DamageModelComponent") or {}) do
-	local max_hp = tonumber(ComponentGetValue2(damagemodel, "max_hp")) or 0
-	ComponentSetValue2(damagemodel, "max_hp", max_hp * 1.01)
+	local max_hp = tonumber(ComponentGetValue2(damagemodel, "max_hp"))
+	local hp = tonumber(ComponentGetValue2(damagemodel, "hp"))
+
+	local new_max_hp = max_hp * 1.01^(1/60) --increase max hp by 1% every second
+	hp = math.max(hp-(new_max_hp-max_hp), 0.4) --lose HP equal to the Max HP gained, down to 10 HP
+
+	ComponentSetValue2(damagemodel, "max_hp", new_max_hp)
+	ComponentSetValue2(damagemodel, "hp", hp)
 end
--- files/chemical_curiosities/materials/morphine/effect_morphine.lua
+++ files/chemical_curiosities/materials/morphine/effect_morphine.lua
@@ -15,12 +15,10 @@ local hp
 max_hp = ComponentGetValue2(damagemodel, "max_hp")
 hp = ComponentGetValue2(damagemodel, "hp"	)
 
-local damage_taken = max_hp - hp
-
 
 
 local function take_damage(dmg)
-	if dmg > 0 then dmg = dmg * .25 end --morphine provides 75% damage resistance (discounted if HP is below 0)
+	if dmg > 0 then dmg = dmg * .25 end --morphine provides 75% damage resistance (discounted if damage is below 0)
 	true_hp = true_hp - dmg
 
 	if true_hp <= 0 then --if the player's true HP is equal to or below 0, trigger kill effect
@@ -42,5 +40,5 @@ function damage_about_to_be_received(incoming_damage, x, y, attacker, crit)
 	take_damage(incoming_damage)
 end
 
-
+local damage_taken = max_hp - hp
 if damage_taken ~= 0 then take_damage(damage_taken) end
