69e99c74e19569d48e6c3439a312b0739603c741
UserK <111083281+FatalUserK@users.noreply.github.com>
2026-01-27 00:16:51 +0000

    fixed s2p fix causing spell to entirely not work

--- files/modify_vanilla.lua
+++ files/modify_vanilla.lua
@@ -5,7 +5,7 @@ nxml.error_handler = function() end
 
 --s2p fix for cpand and just in general
 ModTextFileSetContent("data/scripts/projectiles/spells_to_power.lua",
-    "local ComponentObjectSetValue = ComponentObjectSetValue2/n" ..
+    "local ComponentObjectSetValue = ComponentObjectSetValue2\n\n" ..
     ModTextFileGetContent("data/scripts/projectiles/spells_to_power.lua")
 )
 
