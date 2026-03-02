cdeeaaa47e939be43b1d1bc568b4e914224c8b5a
UserK <0.fatal.userk@gmail.com>
2026-03-02 13:19:41 +0000

    minor work on procedural material spells

--- files/materials_spells.lua
+++ files/materials_spells.lua
@@ -1,8 +1,21 @@
 local CC = {
-	clouds = {
+	cloud = {
 		{
 			material = "cc_dormant_crystal_molten",
 			cosmetic = "cc_dormant_crystal",
+			uses = 15,
+			author = "$cc_name_squirrelly",
+			origin = "$cc_branch_name_cc"
 		}
 	}
 }
+
+
+
+local cloud_default = {
+	price = 140, --Water: 140, Oil: 100, Blood: 200, Acid: 180
+	mana = 30,
+	max_uses = 10,
+	cast_delay = 15
+}
+
